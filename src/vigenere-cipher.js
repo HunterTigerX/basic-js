b579489600a0e4fc24a9aab95796e694992030e1
HunterTigerX <HunterTigerX@gmail.com>
2022-10-17 23:19:35 +0300

    new

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,95 +20,135 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
  class VigenereCipheringMachine {
-
   constructor(mode = true) {
-      this.mode = mode;
-    }
+    this.mode = mode;
+  }
   encrypt(value, key) {
-    throw new NotImplementedError('Not implemented');
-  if (value == null || key == null) {
-    throw Error("Incorrect arguments!");
-  }
-  if (key.length < value.length) {
-    for (let i = 0; i < value.length; i++) {
-      key = key += key[i];
+    if (value == null || key == null) {
+      throw Error("Incorrect arguments!");
     }
-  }
-  let result = [];
-  let y = 0;
-  for (let i = 0; i < value.length; i++) {
-    let letterOne = value[i].toUpperCase().charCodeAt(0) - 64;
-    if (letterOne <= 26 && letterOne > 0) {
-      let letterTwo = key[y].toUpperCase().charCodeAt(0) - 64;
-      let letterThree;
-      if (letterOne + letterTwo > 26) {
-        letterThree = String.fromCharCode(
-          letterOne + letterTwo - 26 - 1 + 64
-        );
-        y++;
-        result.push(letterThree);
-      } else {
-        letterThree = String.fromCharCode(letterOne + letterTwo - 1 + 64);
-        y++;
-        result.push(letterThree);
-      }
-    } else if (letterOne <= 0 || letterOne > 26) {
-      result.push(value[i]);
-    }
-  }
-  if (this.mode === false) {
-      console.log(result.reverse().join(""));
-      return result.reverse().join("");
-    } else {
-      console.log(result.join(""));
-      return result.join("");
-  }
-
-}
-decrypt(value, key) {
-  throw new NotImplementedError('Not implemented');
-  if (value == null || key == null) {
-    throw Error("Incorrect arguments!");
-  }
-  if (key.length < value.length) {
-    for (let i = 0; i < value.length; i++) {
+    if (key.length < value.length) {
+      for (let i = 0; i < value.length; i++) {
         key = key += key[i];
       }
+    } else if (key.length > value.length) {
+      for (let i = 0; key.length > value.length; i++) {
+        key = key
+          .split("")
+          .splice(0, key.length - 1)
+          .join("");
+      }
+    }
 
-  }
-  let result = [];
-  let y = 0;
-  for (let i = 0; i < value.length; i++) {
-    let targetLetter = value[i].toUpperCase().charCodeAt(0) - 64;
-    let keyPosition = key[y].toUpperCase().charCodeAt(0) - 64;
-    let position = targetLetter;
-    let count = 0;
-    if (targetLetter <= 26 && targetLetter > 0) {
-      y++;
-        for (let i = 1; position !== keyPosition; i++) {
-          console.log(position, keyPosition)
-        count++;
-        keyPosition++;
-        if (keyPosition === 26 && position !== 26) {
-          keyPosition = 1;
-          count++;
+    if (this.mode === false) {
+      let result = [];
+      let y = 0;
+      value = value.split("").reverse().join("");
+      for (let i = 0; i < value.length; i++) {
+        let letterOne = value[i].toUpperCase().charCodeAt(0) - 64;
+        if (letterOne <= 26 && letterOne > 0) {
+          let letterTwo = key[y].toUpperCase().charCodeAt(0) - 64;
+          let letterThree;
+          if (letterOne + letterTwo - 1 > 26) {
+            letterThree = String.fromCharCode(
+              letterOne + letterTwo - 26 - 1 + 64
+            );
+            y++;
+            result.push(letterThree);
+          } else {
+            letterThree = String.fromCharCode(letterOne + letterTwo - 1 + 64);
+            y++;
+            result.push(letterThree);
+          }
+        } else if (letterOne <= 0 || letterOne > 26) {
+          result.push(value[i]);
         }
       }
-      result.push(String.fromCharCode(count + 1 + 64));
+      return result.reverse().join("");
     } else {
-      result.push(value[i]);
+      let result = [];
+      let y = 0;
+      for (let i = 0; i < value.length; i++) {
+        let letterOne = value[i].toUpperCase().charCodeAt(0) - 64;
+        if (letterOne <= 26 && letterOne > 0) {
+          let letterTwo = key[y].toUpperCase().charCodeAt(0) - 64;
+          let letterThree;
+          if (letterOne + letterTwo - 1 > 26) {
+            letterThree = String.fromCharCode(
+              letterOne + letterTwo - 26 - 1 + 64
+            );
+            y++;
+            result.push(letterThree);
+          } else {
+            letterThree = String.fromCharCode(letterOne + letterTwo - 1 + 64);
+            y++;
+            result.push(letterThree);
+          }
+        } else if (letterOne <= 0 || letterOne > 26) {
+          result.push(value[i]);
+        }
+      }
+      return result.join("");
     }
   }
 
+  decrypt(value, key) {
+    if (value == null || key == null) {
+      throw Error("Incorrect arguments!");
+    }
+    if (key.length < value.length) {
+      for (let i = 0; i < value.length; i++) {
+        key = key += key[i];
+      }
+    } else if (key.length > value.length) {
+      for (let i = 0; key.length > value.length; i++) {
+        key = key
+          .split("")
+          .splice(0, key.length - 1)
+          .join("");
+      }
+    }
+    let result = [];
+    let y = 0;
+    for (let i = 0; i < value.length; i++) {
+      let targetLetter = value[i].toUpperCase().charCodeAt(0) - 64;
+      let keyPosition = key[y].toUpperCase().charCodeAt(0) - 64;
+      let position = targetLetter;
+      let count = 0;
+      if (targetLetter <= 26 && targetLetter > 0) {
+        y++;
+        for (let i = 1; position !== keyPosition; i++) {
+          if (keyPosition === 26) {
+            count++;
+            keyPosition = 1;
+          } else {
+            count++;
+            keyPosition++;
+            if (keyPosition === 26 && position !== 26) {
+              keyPosition = 1;
+              count++;
+            }
+          }
+        }
+        result.push(String.fromCharCode(count + 1 + 64));
+      } else {
+        result.push(value[i]);
+      }
+    }
+
     if (this.mode === false) {
-      console.log(result.reverse().join(""));
-      return result.reverse().join("");
-    } else {
-      console.log(result.join(""));
       return result.join("");
+    } else {
+      return result.join("");
+    }
   }
 }
-}
+
+
+
+
+
+
 
 module.exports = {
   VigenereCipheringMachine,
